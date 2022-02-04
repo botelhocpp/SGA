873017dcb3230e715d9f1d618863a47e9859c6c0
Daniel Vitor <ripsea.rpg321@gmail.com>
2022-02-04 20:42:30 -0300

    Teste

--- src/br/com/sga/teste.java
+++ src/br/com/sga/teste.java
@@ -1,6 +1,7 @@
 package br.com.sga;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import br.com.sga.datehelper.DateHelper;
@@ -12,40 +13,46 @@ import br.com.sga.pessoal.Sexo;
 
 public class teste {
   public static void main(String args[]) {
-    GerenciadorAlunos ger = null;
-    try {
-      ger = new GerenciadorAlunos("database/Aluno.bin");
+    
+    DateHelper date01 = new DateHelper(new Date());
+    DateHelper date02 = new DateHelper(new Date());
+
+    System.out.println(date01.compareTo(date02));
+
+    // GerenciadorAlunos ger = null;
+    // try {
+    //   ger = new GerenciadorAlunos("database/Aluno.bin");
       
-    } catch (IOException e) {
-      System.out.println("Houve um erro ao abrir o arquivo informado!");
-    }
+    // } catch (IOException e) {
+    //   System.out.println("Houve um erro ao abrir o arquivo informado!");
+    // }
 
-    ger.criarAluno(new Aluno("nome", "1234-1234", Sexo.MASCULINO, "07026278363", new DateHelper("31/10/1999"), "em@ail", new Endereco(), 1));
+    // ger.criarAluno(new Aluno("nome", "1234-1234", Sexo.MASCULINO, "07026278363", new DateHelper("31/10/1999"), "em@ail", new Endereco(), 1));
 
-    List<Aluno> alunos = ger.listarAlunos();
+    // List<Aluno> alunos = ger.listarAlunos();
 
-    System.out.println(alunos.size());
+    // System.out.println(alunos.size());
 
-    for (Aluno aluno : alunos) {
-      System.out.println(aluno);
-    }
+    // for (Aluno aluno : alunos) {
+    //   System.out.println(aluno);
+    // }
 
-    ger.salvarDados();
+    // ger.salvarDados();
 
-    GerenciadorAlunos ger2 = null;
-    try {
-      ger2 = new GerenciadorAlunos("database/Aluno.bin");
+    // GerenciadorAlunos ger2 = null;
+    // try {
+    //   ger2 = new GerenciadorAlunos("database/Aluno.bin");
       
-    } catch (IOException e) {
-      System.out.println("Houve um erro ao abrir o arquivo informado!");
-    }
+    // } catch (IOException e) {
+    //   System.out.println("Houve um erro ao abrir o arquivo informado!");
+    // }
 
-    alunos = ger2.listarAlunos();
+    // alunos = ger2.listarAlunos();
 
-    System.out.println(alunos.size());
+    // System.out.println(alunos.size());
 
-    for (Aluno aluno : alunos) {
-      System.out.println(aluno);
-    }
+    // for (Aluno aluno : alunos) {
+    //   System.out.println(aluno);
+    // }
   }
 }
